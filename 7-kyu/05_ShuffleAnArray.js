64eb0e05208b1e523109dc285ca8e775e842d8dc
ilya <rudakov.alhan@gmail.com>
2022-11-09 18:24:23 +0300

    feat: add solution task Shuffle an Array

---
+++ 7-kyu/05_ShuffleAnArray.js
@@ -0,0 +1,19 @@
+/* task: Shuffle an Array
+
+Write a function to shuffle an array.
+
+Ex.:
+
+Input: [1,2,3,4]
+Output: [3,1,4,2]
+Assume input is array.
+
+Hint: Math.random()
+
+*/
+
+function shuffle(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+console.log(shuffle([1, 2, 3, 4]));
