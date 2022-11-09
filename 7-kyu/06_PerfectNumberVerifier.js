62f5309f7e9ac7d6fd046558897d7021676ad2da
ilya <rudakov.alhan@gmail.com>
2022-11-09 18:48:38 +0300

    feat: add solution Perfect Number Verifier

---
+++ 7-kyu/06_PerfectNumberVerifier.js
@@ -0,0 +1,24 @@
+/* task: Perfect Number Verifier
+
+A perfect number is a number in which the sum of its divisors (excluding itself) are equal to itself.
+
+Write a function that can verify if the given integer n is a perfect number, and return True if it is, or return False if not.
+
+Examples
+n = 28 has the following divisors: 1, 2, 4, 7, 14, 28
+
+1 + 2 + 4 + 7 + 14 = 28 therefore 28 is a perfect number, so you should return True
+
+Another example:
+
+n = 25 has the following divisors: 1, 5, 25
+
+1 + 5 = 6 therefore 25 is not a perfect number, so you should return False
+
+*/
+
+function isPerfect(n) {
+  return [6, 28, 496, 8128, 33550336, 8589869056, 137438691328].includes(n);
+}
+
+console.log(isPerfect(28));
